132debcf5ff514b7f2a544ea02e3bc7ab3481d47
jl-robles <luis.robles731@gmail.com>
2019-11-12 23:21:57 -0600

    first commit

---
+++ src/test/java/com/jlrm/springboot/app/usuarios/commons/ServicioUsuariosCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jlrm.springboot.app.usuarios.commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioUsuariosCommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
